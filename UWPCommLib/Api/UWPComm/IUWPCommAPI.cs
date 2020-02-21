0419dd40ff9c27ce15f203bf7f0de54fe3800476
Joshua Askharoun <jjask7@gmail.com>
2020-02-20 20:32:28 -0800

    Added all current GET endpoints

--- UWPCommLib/Api/UWPComm/IUWPCommAPI.cs
+++ UWPCommLib/Api/UWPComm/IUWPCommAPI.cs
@@ -13,7 +13,6 @@ namespace UWPCommLib.Api.UWPComm
         /// </summary>
         [Get("/projects")]
         Task<List<Project>> GetProjects();
-        Task<List<Project>> GetProjects([Authorize] string token);
 
         /// <summary>
         /// Gets the user's registered projects (requires authentication)
@@ -21,16 +20,18 @@ namespace UWPCommLib.Api.UWPComm
         /// <param name="userId">The Discord ID of the user</param>
         [Get("/user/{userId}/projects")]
         Task<List<Project>> GetUserProjects(string userId);
-        Task<List<Project>> GetUserProjects(string userId, [Authorize] string token);
 
         /// <summary>
         /// Gets the complete list of projects that are registered for the specified Launch year
         /// </summary>
         [Get("/projects/{year}")]
-        Task<List<Project>> GetLaunchProjects(uint year);
-        Task<List<Project>> GetLaunchProjects(uint year, [Authorize] string token);
         Task<List<Project>> GetLaunchProjects(int year);
-        Task<List<Project>> GetLaunchProjects(int year, [Authorize] string token);
+
+        /// <summary>
+        /// Gets the project with the specified ID
+        /// </summary>
+        [Get("/projects/id/{projectId}")]
+        Task<Project> GetProject(int projectId);
 
         /// <summary>
         /// Gets the user's profile information
@@ -38,6 +39,11 @@ namespace UWPCommLib.Api.UWPComm
         /// <param name="userId"></param>
         [Get("/user/{userId}")]
         Task<List<Project>> GetUser(string userId);
-        Task<List<Project>> GetUser(string userId, [Authorize] string token);
+
+        /// <summary>
+        /// Gets the list of collaborators for the specified project
+        /// </summary>
+        [Get("/projects/collaborators?projectId={projectId}")]
+        Task<List<Collaborator>> GetProjectCollaborators(string projectId);
     }
 }
