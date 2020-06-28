ed8c5e5d10c79bcb9804ac321f98a299bdd54299
YoshiAsk <jjask7@gmail.com>
2020-06-28 10:14:32 -0700

    Changed sign in button to icon; Fixed sizing of secondary cards on Home view

--- UWPCommunity/App.xaml.cs
+++ UWPCommunity/App.xaml.cs
@@ -69,12 +69,12 @@ namespace UWPCommunity
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(MainPage), NavigationManager.ParseProtocol(e.Arguments));
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
             }
-            rootFrame.Navigate(typeof(MainPage), NavigationManager.ParseProtocol(e.Arguments));
+            
             SettingsManager.LoadDefaults(false);
             SettingsManager.ApplyAppTheme(SettingsManager.GetAppTheme());
             SettingsManager.ApplyUseDebugApi(SettingsManager.GetUseDebugApi());
