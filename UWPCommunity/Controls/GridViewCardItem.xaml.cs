3d95927b3f385c24d7b00d54ec0418187b679d40
Yoshi Askharoun <jjask7@gmail.com>
2025-01-22 10:13:23 -0500

    [WIP] Image accent colors

--- UWPCommunity/Controls/GridViewCardItem.xaml.cs
+++ UWPCommunity/Controls/GridViewCardItem.xaml.cs
@@ -1,8 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using ColorThiefDotNet;
+using System;
+using System.Collections.Generic;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+using Windows.Storage;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Automation.Provider;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Web.Http;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -10,20 +21,13 @@ namespace UWPCommunity.Controls
 {
     public sealed partial class GridViewCardItem : UserControl, IInvokeProvider
     {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private static readonly ConcurrentDictionary<Uri, Windows.UI.Color> _cachedAccentColors = new ConcurrentDictionary<Uri, Windows.UI.Color>();
+        private Brush _defaultImageBackgroundBrush;
+
         public GridViewCardItem()
         {
-            UseBackdropBlur = SettingsManager.GetUseBlurEffects();
-            SettingsManager.UseBlurEffectsChanged += SettingsManager_UseBlurEffectsChanged;
             this.InitializeComponent();
-            //DataContextChanged += (sender, args) =>
-            //{
-            //    if (args.NewValue != null) Bindings.Update();
-            //};
-        }
-
-        private void SettingsManager_UseBlurEffectsChanged(bool value)
-        {
-            UseBackdropBlur = value;
         }
 
         #region Access Options
@@ -93,7 +97,7 @@ namespace UWPCommunity.Controls
             set => SetValue(ImageSourceProperty, value);
         }
         public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(
-            nameof(ImageSource), typeof(ImageSource), typeof(GridViewCardItem), null);
+            nameof(ImageSource), typeof(ImageSource), typeof(GridViewCardItem), new PropertyMetadata(null, OnImageSourceChanged));
 
         public object BadgeContent
         {
@@ -103,13 +107,13 @@ namespace UWPCommunity.Controls
         public static readonly DependencyProperty BadgeContentProperty = DependencyProperty.Register(
             nameof(BadgeContent), typeof(object), typeof(GridViewCardItem), null);
 
-        public bool UseBackdropBlur
+        public Brush ImageBackgroundBrush
         {
-            get => (bool)GetValue(UseBackdropBlurProperty);
-            set => SetValue(UseBackdropBlurProperty, value);
+            get => (Brush)GetValue(ImageBackgroundBrushProperty);
+            set => SetValue(ImageBackgroundBrushProperty, value);
         }
-        public static readonly DependencyProperty UseBackdropBlurProperty = DependencyProperty.Register(
-            nameof(UseBackdropBlur), typeof(bool), typeof(GridViewCardItem), new PropertyMetadata(true));
+        public static readonly DependencyProperty ImageBackgroundBrushProperty = DependencyProperty.Register(
+            nameof(ImageBackgroundBrush), typeof(Brush), typeof(GridViewCardItem), null);
         #endregion
 
         #region Events
@@ -160,5 +164,84 @@ namespace UWPCommunity.Controls
             );
         }
         #endregion
+
+        private static void OnImageSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            return;
+            var cardItem = (GridViewCardItem)d;
+            if (!(e.NewValue is BitmapImage bitmapImage) || bitmapImage.UriSource == null)
+            {
+                cardItem._defaultImageBackgroundBrush ??= new SolidColorBrush(Colors.Transparent);
+                cardItem.ImageBackgroundBrush = cardItem._defaultImageBackgroundBrush;
+                return;
+            }
+
+            cardItem._defaultImageBackgroundBrush ??= cardItem.ImageBackgroundBrush;
+            var uri = bitmapImage.UriSource;
+
+            Task.Run(async () =>
+            {
+                var accentColor = await GetAccentColorAsync(uri);
+                await cardItem.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Low, () =>
+                {
+                    cardItem.ImageBackgroundBrush = new SolidColorBrush(accentColor);
+                });
+            });
+        }
+
+        private static async Task<Windows.UI.Color> GetAccentColorAsync(Uri uri)
+        {
+            if (_cachedAccentColors.TryGetValue(uri, out var accentColor))
+                return accentColor;
+
+            try
+            {
+                // Open the URI as a stream
+                IRandomAccessStream stream;
+                if (uri.Scheme == "http" || uri.Scheme == "https")
+                {
+                    var response = await _httpClient.GetAsync(uri);
+                    response.EnsureSuccessStatusCode();
+
+                    var inputStream = await response.Content.ReadAsInputStreamAsync();
+
+                    int streamLength = -1;
+                    if (response.Content.TryComputeLength(out var longStreamLength))
+                        streamLength = Convert.ToInt32(longStreamLength);
+
+                    var memStream = new MemoryStream(streamLength);
+                    await inputStream.AsStreamForRead().CopyToAsync(memStream);
+                    stream = memStream.AsRandomAccessStream();
+
+                    inputStream.Dispose();
+                }
+                else
+                {
+                    var file = await StorageFile.GetFileFromApplicationUriAsync(uri);
+                    stream = await file.OpenAsync(FileAccessMode.Read);
+                }
+
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                var thief = new ColorThief();
+                var quantizedAccentColor = await thief.GetColor(decoder);
+                stream.Dispose();
+
+                accentColor = new Windows.UI.Color
+                {
+                    A = quantizedAccentColor.Color.A,
+                    R = quantizedAccentColor.Color.R,
+                    G = quantizedAccentColor.Color.G,
+                    B = quantizedAccentColor.Color.B,
+                };
+            }
+            catch
+            {
+                accentColor = Colors.Transparent;
+            }
+            
+            _cachedAccentColors.TryAdd(uri, accentColor);
+            return accentColor;
+        }
     }
 }
